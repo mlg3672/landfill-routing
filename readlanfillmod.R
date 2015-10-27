82f93a32d68e7e8e2c1339ddbe9e2871828948ad
mlg3672 <michele.goe@gmail.com>
2015-10-27 14:42:56 -0400

    added start points, plot start points add to distance function

--- readlanfillmod.R
+++ readlanfillmod.R
@@ -10,7 +10,8 @@ txt <- readLines("uslandfill_mod.csv") # reads each line as txt string - not hel
 header<-txt[1] # gets header
 tab<-read.table("uslandfill_mod.csv", fill=T) # misread  incomplete columns
 datl<-fread("uslandfill_mod.csv", sep=",") # worked put values in table lumps txt 
-dat<-read.csv("uslandfill_mod.csv", colClasses="character")# reads to df - best option !
+dat<-read.csv("uslandfill_mod.csv", colClasses="character")# reads destination  points to df - best option !
+dato<-read.csv("openpv.csv", colClasses= "character") # read start points to df
 
 # clean data ----------------
 ## set column names
@@ -18,8 +19,15 @@ colnames(dat)<- c("name","city","county","state","zip")
 dats<-dat[,1:5]
 dim(dats)
 
+## subset data
+datos<-dato[,c(2:3,6:8)]
+colnames(datos)<-c("state","size","lat","lon","zip")
+dim(datos)
+
 ## check that all zip codes have 5 digits
 dat[nchar(dat[,5])!=5,]#MA has 4 digits - fixed!
+
+## find NA values
 NAzip<-is.na(dat$zip)#2493
 NAstate<-is.na(dat$state)#0
 NAcity<-is.na(dat$city)#1939
@@ -32,6 +40,9 @@ dat<-dat[dat$state!="GU",]
 dat<-dat[dat$state!="MP",]
 dat<-dat[dat$state!="AS",]
 unique(dat$state)
+
+datos<-datos[datos$state!="PR",]
+unique(datos$state)
 
 # import Alaska data
 AKcases<-read.csv("AK_landfills.csv",colClasses = "character")
@@ -72,17 +83,21 @@ XYpairs <- data.frame(lon=wcitygis$lon,lat=wcitygis$lat)
 plot(XYpairs)
 dim(XYpairs)
 
+Ypairs <-data.frame(lon=datos$lon,lat=datos$lat)
+
+
+
 # find distance between points
 routes<-data.frame(distance=rep(0,dim(XYpairs)[1]-1))
-for (x in 2:dim(XYpairs)[1]) {
-  from=as.numeric(XYpairs[x,])
-  to=as.numeric(XYpairs[x-1,])
+for (x in 1:dim(XYpairs)[1]) {
+  from=as.numeric(Ypairs[x,])
+  to=as.numeric(XYpairs[x,])
   distance <- mapdist(from, to, mode="driving", output="simple")
-  routes$from.lon[x-1]<-from[1]
-  routes$from.lat[x-1]<-from[2]
-  routes$to.lon[x-1]<-to[1]
-  routes$to.lat[x-1]<-to[2]
-  routes$distance[x-1]<-distance$miles
+  routes$from.lon[x]<-from[1]
+  routes$from.lat[x]<-from[2]
+  routes$to.lon[x]<-to[1]
+  routes$to.lat[x]<-to[2]
+  routes$distance[x]<-distance$miles
 }
 
 # write data to csv
