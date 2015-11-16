b50b699ccb5e066384b4109e98f56f7640924a5f
mlg3672 <michele.goe@gmail.com>
2015-11-16 01:16:49 -0500

    added recycling energy values by state

--- optimization_semsc.py
+++ optimization_semsc.py
@@ -8,53 +8,50 @@ import numpy as np
 # to do : 1. add co2 calc to cost function
 #            A. calculate route co2
 #               1. add installation mass estimate - done
-#               2. add recycling energy estimate
+#               2. add recycling energy estimate - done
 #            B. calculate pre-route co2
-#         2. trips calc for transportation
-#            A. add trips variable
-#            B. trips as a multiple interger for capacity
+#         2. trips calc for transportation - done
+#            A. add trips variable - done
+#            B. trips as a multiple interger for capacity- done
 #         3. visualize results
 #            A. map: red points start, green pts end
 #            B. pie chart share of facilities co2
 #            C. bar chart share of facilities mass
 #         4. add item, destination and capacity automatically - done
-# item = (name,origin, size)
- 
-# price = distance
 
-#item = [("AAJ", 10859.348 ),
-#        ("AAI",23278.949662),
-#        ("AAH" ,8692.9),
-#        ("AAG", 20906.12),
-#        ("AAF" ,4604.964),
-#        ("AAE" ,18203.811),
-#        ("AAD" ,17360.872),
-#        ("AAC" ,31590.422),
-#        ("AAB",39549.43),
-#        ("AAA",4154.589)]
-#capacity = {('1'):[150e4],
-#          ('2'):[300e4],
-#           ('3'):[150e4],
-#          ('4'):[300e4],
-#           ('5'):[15e4]}
-
-#destination='RG4'# region
 # constants
-carbon_intensity=73.15 #kgco2 per MMBTU
+carbon_intensity=.07315 #mtco2 per MMBTU
 hv=0.128450 #MMBTU per gal diesel
-mpg_ton=55 #miles-tons per gal
-cap=8.39 #truck capacity in metric tons (18500 lbs)
+mpg=11 #miles per gal
+cap=1.10231e8 #truck capacity in metric tons (50,000 lbs)
 km_per_mile=1.609 #km per mile
+recy=0.005 # recycling energy MWh/kg recovered
+recov=0.2 # percent recovered
+ton_to_lbs=2000
+MWh_to_kWh=1000
+
+efactors = {}
+with open('emissionfactors/emissionfactors_lbskwh.csv') as f:
+    next(f)
+    for line in f:
+        state,abbrev,co2,ch4,n20,so2,nox,hg=line.strip().split(',')[0:8]
+        if abbrev != '':
+            efactors.setdefault((abbrev),[])
+            efactors[(abbrev)].append((float(co2),float(ch4),float(n20))) #lbs/kwh
+f.close()
+#print(efactors)
 
 routes={}
 # 
-for line in open('schedule3R2.txt'):# reading routes for optimization purposes df
-    index,origin,dest,land,r_dist,cl_dist,size,installs=line.strip().split(',')
-    routes.setdefault((origin,dest),[])
-    # Add details to the list of possible routes
-    routes[(origin,dest)].append((land,float(r_dist),float(size),float(cl_dist),int(installs)))
-    
-    
+with open('codeR1.txt') as f:# reading routes for optimization purposes df
+    next(f) # skip header
+    for line in f:
+        index,origin,dest,land,r_dist,cl_dist,size,state=line.strip().split(',')
+        if r_dist != 'NA':
+            routes.setdefault((origin,dest),[])
+            # Add details to the list of possible routes
+            routes[(origin,dest)].append((land,float(r_dist),float(cl_dist),float(size),state))
+f.close()
 #print(routes)
 
 item=list()
@@ -68,19 +65,23 @@ destination = list(routes.keys())[1][1]
 capacity = {} # facility capacity
 for d in routes[routekeys[1]]:
     capacity[d[0]]=100e4
-print("capacity is:",capacity)
+#print("capacity is:",capacity)
 
 locations = {}
 
-for line in open('schedule2R2.txt'):# reading codes for mapping purposes
-    nos,f_lon,f_lat,to_lon,to_lat,r_distance,cl_distance,size,nos,to_state=line.strip().split(',')
-    locations.setdefault((f_lon,f_lat),[])
-    # Add details to the list of possible routes
-    locations[(f_lon,f_lat)].append((float(to_lon),float(to_lat),float(r_distance),float(size),float(cl_distance),int(nos)))
+with open('scheduleR1.csv') as f:# reading codes for mapping purposes
+    next(f)
+    for line in f:
+        i,f_lon,f_lat,to_lon,to_lat,r_distance,cl_distance,size,nos,to_state=line.strip().split(',')
+        locations.setdefault((f_lon,f_lat),[])
+        locations[(f_lon,f_lat)].append((float(to_lon.replace('"','')),
+                                         float(to_lat.replace('"','')),
+                                         float(size.replace('"','')),
+                                         int(nos.replace('"','')),
+                                         to_state))
+f.close()
 #print(locations)
     
-#domain= {}
-#domain=[(0,len(capacity)-1)]*len((item)*1) 
 
 def getminutes(t):
     x=time.strptime(t,'%H:%M')
@@ -89,11 +90,10 @@ def getminutes(t):
 def printschedule(r):
     # input schedule for outbound route of each item
     for d in range(int(len(r))):
-        name=item[d][0]
         origin=item[d][0]
         out=routes[(origin,destination)][int(r[d])]
-        print('%10s%10s%10s %5s km%3s' % (name,origin,destination,
-                                                  out[0],out[1]))
+        print('%10s%5s %5s %3skm %5sMW %3s' % (origin,destination,
+                                                  out[0],int(out[1]), int(out[3]/1000), out[4]))
                                                   
 def plotschedule(r):
     lats = []
@@ -122,7 +122,7 @@ def schedulecost(sol):
     totalprice=0
     capfee=0
     cenfee=0
-    
+    elec=0
     weight = {}
     tot={}
     for i in capacity:
@@ -136,17 +136,26 @@ def schedulecost(sol):
         # get the size
         size = item[d][1]
         ## convert size to mass
-        mass=.056*size # metric tons
+        mass=.09411*size #  8 kg/85W * kW in mt
+        #print("mass is:",mass)
         # get trips
         trips=int(mass/cap)
         ## extract the destination id
         ent=outbound[0]
+        # recycling energy depends on state of facility
+        state=outbound[4]
+        #print('state is',state)
+        #print('efactors are',efactors[(state)][0][0])#,efactors[(state)][0][1],efactors[(state)][0][2])
+        co2=efactors[(state)][0][0]*mass*recov*recy*MWh_to_kWh*1000/(ton_to_lbs)# tons co2  
+        ch4=efactors[(state)][0][1]*mass*recov*recy*MWh_to_kWh*1000/(ton_to_lbs)
+        no2=efactors[(state)][0][2]*mass*recov*recy*MWh_to_kWh*1000/(ton_to_lbs)
+        elec+=co2+ch4*25+no2*298 # GWP
         ## assign mass to facility
         weight[ent].append(int(mass))
-        print("weight is",weight)
+   
        
         # Total price is the co2 kg of all outbound routes
-        totalprice+=carbon_intensity*hv*mass*km_per_mile/(outbound[1]*mpg_ton)
+        totalprice+=(carbon_intensity*hv*(trips*outbound[1]+outbound[2]))/(km_per_mile*mpg)#mt
         #totalprice+=returnf[2]
 
     for i in weight: 
@@ -160,12 +169,12 @@ def schedulecost(sol):
         #print(type(capacity[i]),type(tot[i][0]))
         if int(tot[i][0])>int(capacity[i]): 
             # penalize for overcapacity
-            capfee+=100000
+            capfee+=0
         if int(tot[i][0])>0:
             # penalize for decentralization
-            cenfee+=100
-            
-    return totalprice+capfee+cenfee
+            cenfee+=0
+    #print('electricity impact is',int(elec))        
+    return int(totalprice+capfee+cenfee+elec)
 
 def randomoptimize(domain,costf):
     # costf refers to the cost function
